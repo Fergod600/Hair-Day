acb7837cb230f2599ff9dfc5cc9cac4c4159cb9b
fernando <fernandodat600@gmail.com>
2025-03-12 16:26:42 -0300

    Registrando um novo agendamento na API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,6 @@
 import dayjs from "dayjs"
+
+import {scheduleNew} from "../../services/schedule-new.js"
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -10,7 +12,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   //Previne o comportamento padrão de carregar a página 
   event.preventDefault()
 try {
@@ -37,6 +39,11 @@ try {
   //Gera um id
   const id = new Date().getTime()
 
+  await scheduleNew({
+    id,
+    name,
+    when,
+  })
   
 } catch (error) {
   alert("Não foi possível realizar o agendamento")
